1a077b285c52261c480f002cce2af4f1bbb83328
Massinissa Mokhtari <mokhtari.massinissa@gmail.com>
2020-09-08 23:34:29 +0200

    Generate displacement from phillips spectrum

---
+++ src/simulation/Constants.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "../math/Vector.hpp"
+
+namespace Ocean {
+
+  constexpr int RESOLUTION = 1024;
+  constexpr float PATCH_SIZE = 8.0f;
+
+  constexpr float WIND_SPEED = 3.5f;
+  constexpr float GRAVITY = 9.81f;
+  Vector2 WIND_DIRECTION = Normalize(Vector2(-1, -1));
+
+} // namespace Ocean
---
+++ src/simulation/HeightDisplacement.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "Constants.hpp"
+#include <math.h>
+#include "../math/Numbers.hpp"
+#include "../math/Complex.hpp"
+#include "../math/Fourier.hpp"
+
+namespace Ocean {
+  Complex *spectrum0 = new Complex[RESOLUTION * RESOLUTION];
+  Complex *spectrum = new Complex[RESOLUTION * RESOLUTION];
+  float *heights = new float[RESOLUTION * RESOLUTION];
+
+  float PhillipsSpectrumCoefs(const Vector2 &k) {
+    float L = WIND_SPEED * WIND_SPEED / GRAVITY;
+    float l = L / 1000.0f;
+
+    float kDotw = Dot(k, WIND_DIRECTION);
+    float k2 = Dot(k, k);
+    if (k2 < 0.000001f)
+      return 0;
+
+    float phillips = expf(-1.f / (k2 * L * L)) / (k2 * k2 * k2) * (kDotw * kDotw);
+    // La vague se déplace dans le sens contraire du vent
+    if (kDotw < 0)
+      phillips *= 0.07f;
+
+    return phillips * expf(-k2 * l * l);
+  }
+
+  void GenerateSpectra() {
+    for (int i = 0; i < RESOLUTION; i++)
+      for (int j = 0; j < RESOLUTION; j++) {
+        Vector2 k = M_PI / PATCH_SIZE * Vector2(RESOLUTION - 2 * i, RESOLUTION - 2 * j);
+        float p = sqrtf(PhillipsSpectrumCoefs(k) / 2);
+
+        int index = i * RESOLUTION + j;
+        spectrum0[index] = Complex(RandomGaussian() * p, RandomGaussian() * p);
+      }
+  }
+
+  void UpdateHeights(float t) {
+    for (int i = 0; i < RESOLUTION * RESOLUTION; i++)
+      spectrum[i] = spectrum0[i];
+
+    InverseFourierTransform2D(RESOLUTION, spectrum);
+
+    for (int i = 0; i < RESOLUTION; i++)
+      for (int j = 0; j < RESOLUTION; j++) {
+        float sign = ((i + j) % 2) ? -1 : 1;
+        int index = i * RESOLUTION + j;  
+        heights[index] = sign * spectrum[index].real;
+      }
+  }
+
+} // namespace Ocean
